d7f434fc11687b8b95faecb4ed0103c7339cd931
Jagbir Mangat <jaguarsandhu@gmail.com>
2022-10-29 15:23:43 -0700

    added counter using react

---
+++ RR/7.1.3-declarative-counter/src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import React, { useState } from 'react';
+
+function App() {
+  let [counter, changeCounter] = useState(1)
+  window.changeCounter = changeCounter
+
+  return (
+    <div className="App">
+      <h1>{counter}</h1>
+      <h4>{counter}</h4>
+    </div>
+  );
+}
+
+export default App;
